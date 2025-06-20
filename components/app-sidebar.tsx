a9f410031e5b02a14e738ba52400b45078237d4b
KuyaDark <genovarenanjayson@gmail.com>
2025-06-20 21:31:40 +0800

    Updated Sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -59,7 +59,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="/">
+              <Link href="/dashboard">
                 <div className="flex gap-2 items-center">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
                     <Image
--- components/userSideBar.tsx
+++ components/userSideBar.tsx
@@ -55,7 +55,7 @@ export function UserSideBar({
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="/">
+              <Link href="/dashboardUsers">
                 <div className="flex gap-2 items-center">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
                     <Image
